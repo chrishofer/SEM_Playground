8781b975f776c1a7aad4dfd25056ce709ac064ea
Christian Hofer <christian.hofer@campus02.at>
2020-11-17 17:01:49 +0100

    Added second print

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Strg + Leerzeichen - auto completion
 if __name__ == '__main__':
     print("Hallo DAT")  # Strg + Shift + Enter
+    print("Hallo nocheinmal")
+
     # Shift Shift hintereinander -> Suchbox
     # Strg + shift + v (die letzten paar copy Einträge)
 
