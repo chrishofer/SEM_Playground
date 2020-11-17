244f07174d9cdd241fd3c3c792a3cfae373351de
Christian Hofer <christian.hofer@campus02.at>
2020-11-17 16:57:13 +0100

    Added main file

---
+++ main.py
@@ -0,0 +1,8 @@
+
+# TODO fdsllsal
+# Strg + Leerzeichen - auto completion
+if __name__ == '__main__':
+    print("Hallo DAT")  # Strg + Shift + Enter
+    # Shift Shift hintereinander -> Suchbox
+    # Strg + shift + v (die letzten paar copy Einträge)
+
